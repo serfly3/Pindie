ef1fb98d6a9283109d6d0ddc15f0803b5b5a90a6
serfly3 <153284546+serfly3@users.noreply.github.com>
2024-05-19 21:52:34 +0500

    Update config.js

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://pindie-admin-serfly.nomoredomainswork.ru'
+export const BASE_URL = 'https://pindie-admin-serfly.nomoredomainswork.ru/api'
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
