fea53e4205b0eefd9e35f646fb6acf200770043f
serfly3 <153284546+serfly3@users.noreply.github.com>
2024-05-21 10:53:14 +0500

    Delete app directory

--- app/api/config.js
+++
@@ -1,7 +0,0 @@
-export const BASE_URL = 'https://pindie-admin-serfly.nomoredomainswork.ru/api'
-
-export const endpoints = {
-  games: `${BASE_URL}/games`,
-  auth: `${BASE_URL}/auth/local`,
-  me: `${BASE_URL}/users/me`,
-}
